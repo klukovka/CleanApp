11d9d65575c5cda393c45701c83c0602de974ebd
Yuliia Koba <yuliia.koba@nure.ua>
2022-01-20 00:59:57 +0200

    login ui

---
+++ app/src/main/java/com/example/cleanapp/presentation/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cleanapp.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cleanapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
