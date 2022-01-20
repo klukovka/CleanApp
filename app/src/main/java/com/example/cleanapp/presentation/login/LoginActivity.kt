2d2533da8c7072560135e7c7017ab883765f4bcb
Yuliia Koba <yuliia.koba@proarea.com>
2022-01-20 18:41:05 +0200

    fixes in login activity

--- app/src/main/java/com/example/cleanapp/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/example/cleanapp/presentation/login/LoginActivity.kt
@@ -2,11 +2,33 @@ package com.example.cleanapp.presentation.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.cleanapp.R
+import com.example.cleanapp.data.login.remote.dto.LoginRequest
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+    private val viewModel : LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    private fun login(){
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString().trim()
+            val password = binding.passwordEditText.text.toString().trim()
+            if(validate(email, password)){
+                val loginRequest = LoginRequest(email, password)
+                viewModel.login(loginRequest)
+            }
+        }
+    }
+
+
 }
