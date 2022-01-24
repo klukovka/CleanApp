e12eafe96cff67a36015224cdba9b65dc4d9e362
Yuliia Koba <yuliia.koba@proarea.com>
2022-01-24 17:42:55 +0200

    add main menu

--- app/src/main/java/com/example/cleanapp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/cleanapp/presentation/main/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.cleanapp.presentation.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.cleanapp.R
 import com.example.cleanapp.common.utils.SharedPref
@@ -59,5 +62,24 @@ class MainActivity : AppCompatActivity() {
         goToLoginActivity()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sign_out -> {
+                signOut()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+    
 }
